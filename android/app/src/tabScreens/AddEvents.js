22343c9bc5cddefce812adca780249c04b8cdd75
Akash <yogibabu32@gmail.com>
2023-11-14 17:39:00 +0530

    added firestore fun, form for adding events,

--- android/app/src/tabScreens/AddEvents.js
+++ android/app/src/tabScreens/AddEvents.js
@@ -8,91 +8,302 @@ import {
   TouchableOpacity,
   DatePickerAndroid,
   Image,
+  Alert,
 } from 'react-native';
-
+import firestore from '@react-native-firebase/firestore';
 const AddEventForm = () => {
   const [eventName, setEventName] = useState('');
   const [eventDescription, setEventDescription] = useState('');
   const [eventLocation, setEventLocation] = useState('');
+  const [eventDate, setEventDate] = useState('');
+  const [ticketPrice, setTicketPrice] = useState('');
+  const [organizer, setOrganizer] = useState('');
+  const [contactPhone, setContactPhone] = useState('');
+  const isFormValid = () => {
+    return (
+      eventName.trim() !== '' &&
+      eventDescription.trim() !== '' &&
+      eventLocation.trim() !== '' &&
+      eventDate.trim() !== '' &&
+      ticketPrice.trim() !== '' &&
+      organizer.trim() !== '' &&
+      contactPhone.trim() !== ''
+    );
+  };
 
-  const handleAddEvent = () => {};
+  const handleAddEvent = async () => {
+    try {
+      await firestore().collection('events').add({
+        eventName: eventName,
+        eventDescription: eventDescription,
+        eventLocation: eventLocation,
+        eventDate: eventDate,
+        ticketPrice: ticketPrice,
+        organizer: organizer,
+        contactPhone: contactPhone,
+      });
 
+      Alert.alert('Success', 'Event added to Firestore!');
+    } catch (error) {
+      Alert.alert(
+        'Error',
+        'Error adding event to Firestore. Please try again.',
+      );
+    } finally {
+      setEventName('');
+      setEventDescription('');
+      setEventLocation('');
+      setEventDate('');
+      setTicketPrice('');
+      setOrganizer('');
+      setContactPhone('');
+    }
+  };
+
+  const handleCancelEventName = () => {
+    setEventName('');
+  };
+  const handleCancelDescription = () => {
+    setEventDescription('');
+  };
+  const handleCancelLocation = () => {
+    setEventLocation('');
+  };
+  const handleCancelEventDate = () => {
+    setEventDate('');
+  };
+  const handleCancelTcketPrice = () => {
+    setTicketPrice('');
+  };
+  const handleCancelOrganizer = () => {
+    setOrganizer('');
+  };
+  const handleCancelPhone = () => {
+    setContactPhone('');
+  };
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
-        <View style={styles.inputFlex}>
-          <View>
-            <TextInput
-              style={styles.input}
-              placeholder=" Enter Event Name"
-              //   placeholderTextColor="black"
-              value={eventName}
-              onChangeText={text => setEventName(text)}
-            />
-          </View>
-
-          {eventName.length > 0 && (
-            <View>
-              <TouchableOpacity>
-                <Image
-                  source={require('../Image/cancel.jpg')}
-                  style={styles.cancle}
-                />
-              </TouchableOpacity>
+        <View style={styles.borderBox}>
+          <View style={styles.inputFlex}>
+            <View style={styles.inputIcon}>
+              <Image
+                source={require('../Image/event.png')}
+                style={styles.icons}
+              />
             </View>
-          )}
+            <View>
+              <TextInput
+                style={styles.input}
+                placeholder=" Enter Event Name"
+                //   placeholderTextColor="black"
+                value={eventName}
+                onChangeText={text => setEventName(text)}
+              />
+            </View>
+
+            {eventName.length > 0 && (
+              <View>
+                <TouchableOpacity onPress={handleCancelEventName}>
+                  <Image
+                    source={require('../Image/cancel.jpg')}
+                    style={styles.cancle}
+                  />
+                </TouchableOpacity>
+              </View>
+            )}
+          </View>
         </View>
 
-        <View style={styles.inputFlex}>
-          <View>
-            <TextInput
-              style={styles.input}
-              placeholder=" Enter Event Description"
-              //   placeholderTextColor="black"
-              value={eventDescription}
-              onChangeText={text => setEventDescription(text)}
-            />
-          </View>
-
-          {eventDescription.length > 0 && (
-            <View>
-              <TouchableOpacity>
-                <Image
-                  source={require('../Image/cancel.jpg')}
-                  style={styles.cancle}
-                />
-              </TouchableOpacity>
+        <View style={styles.borderBox}>
+          <View style={styles.inputFlex}>
+            <View style={styles.inputIcon}>
+              <Image
+                source={require('../Image/description.png')}
+                style={styles.icons}
+              />
             </View>
-          )}
-        </View>
-        <View style={styles.inputFlex}>
-          <View>
-            <TextInput
-              style={styles.input}
-              placeholder=" Enter Event eventLocation"
-              //   placeholderTextColor="black"
-              value={eventLocation}
-              onChangeText={text => setEventLocation(text)}
-            />
-          </View>
-
-          {eventLocation.length > 0 && (
             <View>
-              <TouchableOpacity>
-                <Image
-                  source={require('../Image/cancel.jpg')}
-                  style={styles.cancle}
-                />
-              </TouchableOpacity>
+              <TextInput
+                style={styles.input}
+                placeholder=" Enter Event Description"
+                //   placeholderTextColor="black"
+                value={eventDescription}
+                onChangeText={text => setEventDescription(text)}
+              />
             </View>
-          )}
+
+            {eventDescription.length > 0 && (
+              <View>
+                <TouchableOpacity onPress={handleCancelDescription}>
+                  <Image
+                    source={require('../Image/cancel.jpg')}
+                    style={styles.cancle}
+                  />
+                </TouchableOpacity>
+              </View>
+            )}
+          </View>
         </View>
 
-        <Button
-          title="Add Event"
-          onPress={handleAddEvent}
-          style={{marginTop: '30'}}
-        />
+        <View style={styles.borderBox}>
+          <View style={styles.inputFlex}>
+            <View style={styles.inputIcon}>
+              <Image
+                source={require('../Image/eventlocation.png')}
+                style={styles.icons}
+              />
+            </View>
+            <View>
+              <TextInput
+                style={styles.input}
+                placeholder=" Enter Event Location"
+                //   placeholderTextColor="black"
+                value={eventLocation}
+                onChangeText={text => setEventLocation(text)}
+              />
+            </View>
+
+            {eventLocation.length > 0 && (
+              <View>
+                <TouchableOpacity onPress={handleCancelLocation}>
+                  <Image
+                    source={require('../Image/cancel.jpg')}
+                    style={styles.cancle}
+                  />
+                </TouchableOpacity>
+              </View>
+            )}
+          </View>
+        </View>
+        <View style={styles.borderBox}>
+          <View style={styles.inputFlex}>
+            <View style={styles.inputIcon}>
+              <Image
+                source={require('../Image/date.png')}
+                style={styles.icons}
+              />
+            </View>
+            <View>
+              <TextInput
+                style={styles.input}
+                placeholder=" DD/MM/YY"
+                //   placeholderTextColor="black"
+                value={eventDate}
+                onChangeText={text => setEventDate(text)}
+              />
+            </View>
+
+            {eventDate.length > 0 && (
+              <View>
+                <TouchableOpacity onPress={handleCancelEventDate}>
+                  <Image
+                    source={require('../Image/cancel.jpg')}
+                    style={styles.cancle}
+                  />
+                </TouchableOpacity>
+              </View>
+            )}
+          </View>
+        </View>
+        <View style={styles.borderBox}>
+          <View style={styles.inputFlex}>
+            <View style={styles.inputIcon}>
+              <Image
+                source={require('../Image/ticketprice.png')}
+                style={styles.icons}
+              />
+            </View>
+            <View>
+              <TextInput
+                style={styles.input}
+                placeholder=" ticket Price"
+                //   placeholderTextColor="black"
+                value={ticketPrice}
+                onChangeText={text => setTicketPrice(text)}
+              />
+            </View>
+
+            {ticketPrice.length > 0 && (
+              <View>
+                <TouchableOpacity onPress={handleCancelTcketPrice}>
+                  <Image
+                    source={require('../Image/cancel.jpg')}
+                    style={styles.cancle}
+                  />
+                </TouchableOpacity>
+              </View>
+            )}
+          </View>
+        </View>
+        <View style={styles.borderBox}>
+          <View style={styles.inputFlex}>
+            <View style={styles.inputIcon}>
+              <Image
+                source={require('../Image/organizer.png')}
+                style={styles.icons}
+              />
+            </View>
+            <View>
+              <TextInput
+                style={styles.input}
+                placeholder=" organizer"
+                //   placeholderTextColor="black"
+                value={organizer}
+                onChangeText={text => setOrganizer(text)}
+              />
+            </View>
+
+            {organizer.length > 0 && (
+              <View>
+                <TouchableOpacity onPress={handleCancelOrganizer}>
+                  <Image
+                    source={require('../Image/cancel.jpg')}
+                    style={styles.cancle}
+                  />
+                </TouchableOpacity>
+              </View>
+            )}
+          </View>
+        </View>
+        <View style={styles.borderBox}>
+          <View style={styles.inputFlex}>
+            <View style={styles.inputIcon}>
+              <Image
+                source={require('../Image/phone.png')}
+                style={styles.icons}
+              />
+            </View>
+            <View>
+              <TextInput
+                style={styles.input}
+                placeholder=" Phone number"
+                //   placeholderTextColor="black"
+                value={contactPhone}
+                onChangeText={text => setContactPhone(text)}
+              />
+            </View>
+
+            {contactPhone.length > 0 && (
+              <View>
+                <TouchableOpacity onPress={handleCancelPhone}>
+                  <Image
+                    source={require('../Image/cancel.jpg')}
+                    style={styles.cancle}
+                  />
+                </TouchableOpacity>
+              </View>
+            )}
+          </View>
+        </View>
+
+        <View style={{marginTop: 30}}>
+          <Button
+            title="Add Event"
+            onPress={handleAddEvent}
+            disabled={!isFormValid()}
+          />
+        </View>
       </View>
     </View>
   );
@@ -113,30 +324,46 @@ const styles = StyleSheet.create({
   inputContainer: {
     width: '100%',
   },
+  borderBox: {
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: '#000',
+    borderRadius: 10,
+    padding: 5,
+    marginBottom: 20,
+  },
+  inputIcon: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 5,
+  },
   input: {
     height: 40,
     width: 330,
-    paddingHorizontal: 10,
-    marginBottom: 10,
+    paddingHorizontal: 15,
+
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontWeight: '100',
   },
   cancle: {
     width: 20,
-    height: 20,
+    height: 25,
     marginTop: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 20,
+  },
+  icons: {
+    width: 20,
+    height: 20,
+    // marginTop: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 
   multilineInput: {
     height: 80,
-  },
-  datePicker: {
-    height: 40,
-    borderColor: '#ddd',
-    borderWidth: 1,
-    marginBottom: 16,
-    padding: 8,
-    borderRadius: 4,
-    justifyContent: 'center',
-    backgroundColor: '#f9f9f9',
   },
 });
 
--- android/app/src/tabScreens/Setting.js
+++ android/app/src/tabScreens/Setting.js
@@ -1,12 +1,67 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Button,
+  Modal,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+  ActivityIndicator,
+  NativeEventEmitter,
+  Alert,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {useNavigation} from '@react-navigation/native';
+
+import auth from '@react-native-firebase/auth';
 
 const Setting = () => {
+  const navigation = useNavigation();
+
+  const [isLodingLogout, setIsLodingLogout] = useState(false);
+  //  handleLogout
+  const handleLogout = () => {
+    setIsLodingLogout(true);
+    auth()
+      .signOut()
+      .then(res => {
+        setIsLodingLogout(false);
+        Alert.alert('logged out');
+        navigation.navigate('Login');
+      })
+      .catch(error => {
+        Alert.alert(error);
+      })
+      .finally(() => {
+        setIsLodingLogout(false); // Clear the logout in progress, whether it succeeded or failed
+      });
+  };
   return (
-    <View>
-      <Text>Setting</Text>
+    <View style={{alignSelf: 'flex-end'}}>
+      <View>
+        {isLodingLogout ? (
+          <ActivityIndicator size="large" color="#0000ff" />
+        ) : (
+          <TouchableOpacity onPress={handleLogout}>
+            <Image
+              source={require('../Image/logOut.jpg')}
+              style={styles.logoutButton}
+            />
+          </TouchableOpacity>
+        )}
+      </View>
     </View>
   );
 };
+const styles = StyleSheet.create({
+  logoutButton: {
+    width: 60,
+    height: 60,
+    marginTop: 20,
+    marginRight: 20,
+  },
+});
 
 export default Setting;
